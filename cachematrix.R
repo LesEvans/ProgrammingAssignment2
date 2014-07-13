89878d09b52dd2429b25be7aa302caab91c43dae
LesEvans <LesEvans@users.noreply.github.com>
2014-07-13 19:17:54 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates square matrix, randomly assigned values using Poisson, finally finds and caches the inverse of matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+mmakeCacheMatrix <- function(x = matrix()) {
+  t<-x^2
+  m<-matrix(rpois(t,20),x,x)
+  
+  i <- NULL
+  set <- function(y) {
+    m <<- y
+    i <<- NULL
+  }
+  get<- function() m
+  setinverse <- solve(m)
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
-
 
 ## Write a short comment describing this function
 
